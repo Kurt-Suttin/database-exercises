1257b595ea204198d71140064f9507e5d81b6fbe
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-22 15:15:01 -0500

    finished Find the current titles of employees currently working in the Customer Service department.

--- join_exercises.sql
+++ join_exercises.sql
@@ -80,11 +80,29 @@ SELECT d.dept_name AS "Deparment Name", CONCAT(e.first_name, ' ', e.last_name) A
 FROM employees as e
          JOIN dept_manager as dm ON dm.emp_no = e.emp_no
          JOIN departments as d ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01';
+
+
+SELECT d.dept_name AS "Deparment Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager"
+FROM employees as e
+         JOIN dept_manager as dm ON dm.emp_no = e.emp_no
+         JOIN departments as d ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01'
   AND e.gender = 'F';
 
 
 
+DESCRIBE dept_emp;
+# Find the current titles of employees currently working in the Customer Service department.
 
+
+SELECT t.title AS title , COUNT(de.emp_no) AS total
+From titles t
+JOIN dept_emp de
+ON de.emp_no = t.emp_no
+WHERE de.dept_no = 'd009'
+AND t.to_date = '9999-01-01'
+AND de.to_date = '9999-01-01'
+GROUP BY t.title;
 
 
