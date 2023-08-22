39ac7d74b597311ec9d2bb262b72c7f8db0c91e3
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-08-22 09:28:45 -0500

    order-by exercises completed after walkthrough

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,1 +1,33 @@
 use employees;
+
+SELECT title
+FROM titles
+GROUP BY title;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY first_name, last_name
+ORDER BY last_name ASC;
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+SELECT COUNT(*) AS Total, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+SELECT COUNT(*) AS Total , gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
+
+
